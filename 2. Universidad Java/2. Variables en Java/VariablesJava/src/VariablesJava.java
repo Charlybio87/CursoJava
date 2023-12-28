150f4712e79fc909bbda8e158a6f0acf00b087a4
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2023-12-28 10:38:29 -0300

    Fundamentos de Java
    Variables

---
+++ 2. Universidad Java/2. Variables en Java/VariablesJava/src/VariablesJava.java
@@ -0,0 +1,21 @@
+public class VariablesJava {
+    public static void main(String[] args) {
+        //Definimos la variables
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+
+        //var - Inferencia de tipos en Java
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+        var miVariableCadena2 = "Nueva Cadena";
+        System.out.println("miVariableCadena2 = " + miVariableCadena2); //soutv
+        
+    }
+}
