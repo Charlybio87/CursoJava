dcc094123d60442eebe5ae298e4c22f85b377fee
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2024-01-08 10:47:11 -0300

    Constructor
    AritmeticaV2

---
+++ 2. Universidad Java/9. Constructor/Aritmetica/src/operaciones/Aritmetica.java
@@ -0,0 +1,18 @@
+package operaciones;
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    //Constructor Vacio
+    public Aritmetica() {
+        System.out.println("Ejecutando Constructor");
+    }
+
+    public Aritmetica(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println("Ejecutando constructor " +
+                           "con argumentos");
+    }
+}
