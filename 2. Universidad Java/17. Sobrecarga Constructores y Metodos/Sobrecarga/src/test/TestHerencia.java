186cb1ca5e6df7ee09a98a5c210179659b5899f0
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2024-01-16 09:19:38 -0300

    Sobrecarga Constructor y Método

---
+++ 2. Universidad Java/17. Sobrecarga Constructores y Metodos/Sobrecarga/src/test/TestHerencia.java
@@ -0,0 +1,22 @@
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+//        var fecha = new Date();
+        Empleado empleado1 = new Empleado("Juan", 5000.0);
+        System.out.println("empleado1 = " + empleado1);
+//        Cliente cliente1 = new Cliente(fecha
+//                ,true,"Charly",
+//                'M',36,"Irigoyen 1455");
+//        System.out.println("cliente1 = " + cliente1);
+//        Cliente cliente2 = new Cliente(fecha, true, "Santino",
+//                'M',7,"Irigoyen 1455");
+//        System.out.println("cliente2 = " + cliente2);
+
+    }
+}
